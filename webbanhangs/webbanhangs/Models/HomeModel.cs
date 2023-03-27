7e905039a00f123f7a8e30763cba961b16bb8512
kiento00 <92322738+kiento00@users.noreply.github.com>
2023-03-27 23:00:01 +0700

    ket noi data

---
+++ webbanhangs/webbanhangs/Models/HomeModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using webbanhangs.Context;
+namespace webbanhangs.Models
+{
+    public class HomeModel
+    {
+        public List<LoaiSanPham> ListLoaiSanPham { get; set; }
+        public List<SanPham> ListSanPham { get; set; }
+    }
+}
